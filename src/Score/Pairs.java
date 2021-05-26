b7249a8bc739313908b2a097ab7fdc3ec2510bb3
Tortuise <kennyu@student.unimelb.edu.au>
2021-05-26 17:46:32 +1000

    added runs and pairs

---
+++ src/Score/Pairs.java
@@ -0,0 +1,11 @@
+package Score;
+
+public class Pairs implements ScoringStrategy {
+
+	@Override
+	public int getScore() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/Score/Runs.java
@@ -0,0 +1,11 @@
+package Score;
+
+public class Runs implements ScoringStrategy {
+
+	@Override
+	public int getScore() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
